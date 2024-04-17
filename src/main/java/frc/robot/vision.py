c91317c4e591b87cbee03614c2f29023fe6dafdb
Erebus Sophia Oh <eoh2@swarthmore.edu>
2024-04-17 10:46:58 -0400

    fixed camera settings

--- src/main/java/frc/robot/vision.py
+++ src/main/java/frc/robot/vision.py
@@ -1,116 +1,95 @@
 import json
 import time
 import sys
+import cv2
+import numpy as np
 
-from cscore import CameraServer, VideoSource, UsbCamera, MjpegServer
-from ntcore import NetworkTableInstance, EventFlags
+from cscore import CameraServer, UsbCamera
+import ntcore
 
 configFile = "/boot/frc.json"
 
-class CameraConfig: pass
-
-team = None
-server = False
-cameraConfig = None
-camera = None
-
-def parseError(str):
-    """Report parse error."""
-    print("config error in '" + configFile + "': " + str, file=sys.stderr)
-
-def readCameraConfig(config):
-    """Read single camera configuration."""
-    cameraConfig = CameraConfig()
-
-    # name
-    try:
-        cameraConfig.name = config["name"]
-    except KeyError:
-        parseError("could not read camera name")
-        return False
-
-    # path
-    try:
-        cameraConfig.path = config["path"]
-    except KeyError:
-        parseError("camera '{}': could not read path".format(cameraConfig.name))
-        return False
-
-    # stream properties
-    cameraConfig.streamConfig = config.get("stream")
-
-    cameraConfig.config = config
-
-    return True
-
-def readConfig():
-    """Read configuration file."""
-    global team
-    global server
-
-    # parse file
-    try:
-        with open(configFile, "rt", encoding="utf-8") as f:
-            j = json.load(f)
-    except OSError as err:
-        print("could not open '{}': {}".format(configFile, err), file=sys.stderr)
-        return False
-
-    # top level must be an object
-    if not isinstance(j, dict):
-        parseError("must be JSON object")
-        return False
-
-    # team number
-    try:
-        team = j["team"]
-    except KeyError:
-        parseError("could not read team number")
-        return False
-
-    # ntmode (optional)
-    if "ntmode" in j:
-        str = j["ntmode"]
-        if str.lower() == "client":
-            server = False
-        elif str.lower() == "server":
-            server = True
-        else:
-            parseError("could not understand ntmode value '{}'".format(str))
-
-    # camera
-    try:
-        cameras = j["cameras"]
-    except KeyError:
-        parseError("could not read cameras")
-        return False
-
-    print(cameras)
-    '''[
-        {'brightness': 60, 'exposure': 9, 'fps': 30, 'height': 480, 'name': 'Logitech C920', 'path': '/dev/video0', 'pixel format': 'mjpeg', 'properties': [{'name': 'connect_verbose', 'value': 1}, {'name': 'contrast', 'value': 50}, {'name': 'saturation', 'value': 70}, {'name': 'gain', 'value': 0}, {'name': 'power_line_frequency', 'value': 2}, {'name': 'sharpness', 'value': 50}, {'name': 'backlight_compensation', 'value': 0}, {'name': 'exposure_auto_priority', 'value': False}, {'name': 'pan_absolute', 'value': 0}, {'name': 'tilt_absolute', 'value': 0}, {'name': 'focus_absolute', 'value': 0}, {'name': 'focus_auto', 'value': False}, {'name': 'zoom_absolute', 'value': 100}], 'stream': {'properties': []}, 'white balance': 3300, 'width': 640}
-        ]
-    '''
-
-    readCameraConfig(cameras)
-
-    return True
-
 if __name__ == "__main__":
-    configFile = "/boot/frc.json"
-    readConfig()
+    with open(configFile) as f:
+        config = json.load(f)
 
-    print("1")
+    team = config["team"]
+    camera_config = config['cameras'][0]
 
-    CameraServer.enableLogging()
+    print("Starting camera '{}' on {}".format(camera_config["name"], camera_config["path"]))
 
-    print("Starting camera '{}' on {}".format(cameraConfig.name, cameraConfig.path))
+    nt = ntcore.NetworkTableInstance.getDefault()
 
-    camera = UsbCamera(cameraConfig.name, cameraConfig.path)
-    server = CameraServer.startAutomaticCapture(camera=camera)
+    # camera.getProperty("name").set(val)
+    camera = CameraServer.startAutomaticCapture()
+    camera.setResolution(640, 480)
+    camera.setBrightness(60)
+    camera.setWhiteBalanceManual(3300)
+    camera.setExposureManual(7)
+    camera.getProperty("contrast").set(50)
+    camera.getProperty("saturation").set(80)
+    camera.getProperty("focus_auto").set(0)
 
-    camera.setConfigJson(json.dumps(cameraConfig.config))
+    input_stream = CameraServer.getVideo()
+    output_stream = CameraServer.putVideo("Processed", 640, 480)
 
-    sink = server.getVideo()
+    vision_nt = nt.getTable("Vision")
+
+    img = np.zeros((640, 480, 3), dtype=float)
+
+    time.sleep(0.5)
 
     while True:
-        time, input_img = sink.grabFrame(input_img)
+        start_time = time.time()
+
+        frame_time, input_img = input_stream.grabFrame(img)
+        output_img = np.copy(input_img)
+
+        end_time = time.time()
+
+        process_time = end_time - start_time
+        fps = 1/process_time
+        cv2.putText(output_img, str(round(fps, 1)), (0, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255))
+
+        output_stream.putFrame(output_img)
+
+#   JSON format:
+#   {
+#       "team": <team number>,
+#       "ntmode": <"client" or "server", "client" if unspecified>
+#       "cameras": [
+#           {
+#               "name": <camera name>
+#               "path": <path, e.g. "/dev/video0">
+#               "pixel format": <"MJPEG", "YUYV", etc>   // optional
+#               "width": <video mode width>              // optional
+#               "height": <video mode height>            // optional
+#               "fps": <video mode fps>                  // optional
+#               "brightness": <percentage brightness>    // optional
+#               "white balance": <"auto", "hold", value> // optional
+#               "exposure": <"auto", "hold", value>      // optional
+#               "properties": [                          // optional
+#                   {
+#                       "name": <property name>
+#                       "value": <property value>
+#                   }
+#               ],
+#               "stream": {                              // optional
+#                   "properties": [
+#                       {
+#                           "name": <stream property name>
+#                           "value": <stream property value>
+#                       }
+#                   ]
+#               }
+#           }
+#       ]
+#       "switched cameras": [
+#           {
+#               "name": <virtual camera name>
+#               "key": <network table key used for selection>
+#               // if NT value is a string, it's treated as a name
+#               // if NT value is a double, it's treated as an integer index
+#           }
+#       ]
+#   }
