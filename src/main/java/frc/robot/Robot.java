c91caa03fe59b6b3e2aa44fdcd7cbbddcef88467
Erebus Sophia Oh <eoh2@swarthmore.edu>
2024-04-25 15:04:56 -0400

    network tables working

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -24,6 +24,22 @@ import org.opencv.core.Point;
 import java.util.List;
 import java.util.ArrayList;
 
+import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.cscore.CameraServerJNI;
+import edu.wpi.first.math.WPIMathJNI;
+import edu.wpi.first.networktables.DoubleSubscriber;
+// Raspbarry Pi Vision
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.NetworkTablesJNI;
+import edu.wpi.first.util.CombinedRuntimeLoader;
+import edu.wpi.first.util.WPIUtilJNI;
+import edu.wpi.first.cscore.CameraServerJNI;
+import edu.wpi.first.math.WPIMathJNI;
+import edu.wpi.first.util.WPIUtilJNI;
+import edu.wpi.first.util.CombinedRuntimeLoader;
+import java.io.IOException;
+
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to
@@ -81,6 +97,9 @@ public class Robot extends TimedRobot {
   // Vision Variables
   private static Vision cv;
   private static Constants C = new Constants();
+  DoubleSubscriber redASub;
+  DoubleSubscriber redXSub;
+  DoubleSubscriber redYSub;
 
   enum autoStates {
     findRed, 
@@ -443,8 +462,37 @@ public class Robot extends TimedRobot {
    * Initializes Computer Vision
    */
   private void initVision() {
-    cv = cv.getInstance();
-    cv.startVision();
+
+    System.out.println("robot init");
+
+    // cv = cv.getInstance();
+    // cv.startVision();
+    // NetworkTablesJNI.Helper.setExtractOnStaticLoad(false);
+    // WPIUtilJNI.Helper.setExtractOnStaticLoad(false);
+    // WPIMathJNI.Helper.setExtractOnStaticLoad(false);
+    // CameraServerJNI.Helper.setExtractOnStaticLoad(false);
+    
+    // try {
+    //   CombinedRuntimeLoader.loadLibraries(Robot.class, "wpiutiljni", "wpimathjni", "ntcorejni", "cscorejnicvstatic");
+    // }catch(IOException e) {System.out.println("Rip load libraries failed")}
+
+    // System.out.println("loaded libraries");
+    
+    NetworkTableInstance inst = NetworkTableInstance.getDefault();
+    inst.setServer("6593");
+    
+    NetworkTable table = inst.getTable("Vision");
+    redASub = table.getDoubleTopic("redA").subscribe(-1.0);
+    redXSub = table.getDoubleTopic("redX").subscribe(-1.0);
+    redYSub = table.getDoubleTopic("redY").subscribe(-1.0);
+
+    System.out.println("init network tables");
+
+    // inst.startClient4("java client");
+    // inst.setServer("10.65.93.10");
+    // inst.startDSClient();
+
+    System.out.println("end roboto init");
   }
 
   /*
@@ -536,213 +584,231 @@ public class Robot extends TimedRobot {
     colorOfInterest = Colors.none;
   }
 
-  /** This function is called periodically during autonomous. */
-  @Override
+ // temp netword tables test, comment out eventually - ere
   public void autonomousPeriodic() {
+    // Network Tables - ere
+    double redArea = redASub.get();
+    // double redX = redXSub.get();
+    // double redY = redYSub.get();
 
-    double[] current_rel = new double[] {
-        RM_Encoders.get(WHEEL_FL).getPosition(),
-        RM_Encoders.get(WHEEL_FR).getPosition(),
-        RM_Encoders.get(WHEEL_BR).getPosition(),
-        RM_Encoders.get(WHEEL_BL).getPosition()
-    };
-
-    // Initialize empty arrays for RM and TM setPoint values to be used later by
-    // setReference
-    double[] desired_body = new double[] { 0.0, 0.0, 0.0, 0.0 };
-    double[] desired_rel1 = new double[] { 0.0, 0.0, 0.0, 0.0 };
-    double[] desired_translation = new double[] { 0.0, 0.0, 0.0, 0.0 };
-    double[] DESIRED = new double[] { 0, 0, 0, 0 };
-
-    Point center = cv.rectCenter(cv.biggestRed);
-
-    if (colorOfInterest == Colors.red) {
-      center = cv.rectCenter(cv.biggestRed);
-    }
-    else if (colorOfInterest == Colors.green) {
-      center = cv.rectCenter(cv.biggestGreen);
-    }
-
-    double x = center.x;
-    double centerx = (double) cv.imgWidth / 2;
-    double x_diff = Math.abs(centerx - x);
-    
-    double setpoint = 0;
-    double[] setpointArr = new double[] {setpoint, setpoint, setpoint, setpoint};
-
-    switch (currState) {
-
-      case findRed:
-        System.out.println("currState = findRed ------------------------");
-      
-        colorOfInterest = Colors.red;
-
-        // if nothing of (RED) color detected, keep spinning around until detected
-        if (cv.biggestRed.area() <= C.minSpinThresh) {
-          spin(DESIRED, desired_body, desired_rel1, desired_translation, current_rel);
-        }
-        else {
-          currState = autoStates.move;
-        }
-      break;
-
-      case findGreen:
-        System.out.println("currState = findGreen ------------------------");
-        
-        colorOfInterest = Colors.green;
-
-        // if nothing of (GREEN) color detected, keep spinning around until detected
-        if (cv.biggestGreen.area() <= C.minSpinThresh) {
-          spin(DESIRED, desired_body, desired_rel1, desired_translation, current_rel);
-        }
-        else {
-          currState = autoStates.move;
-        }
-      break;
-
-      case move:
-
-        System.out.println("currState = move ------------------------");
-        System.out.printf("colorOfInterest = %s ------------------------\n", colorOfInterest);
-        System.out.printf("area = %f ------------------------\n", area);
-      
-          
-        if (colorOfInterest == Colors.red) {
-          area = cv.biggestRed.area();
-        }
-        else if (colorOfInterest == Colors.green) {
-          area = cv.biggestGreen.area();
-        }
-        
-        // if not centered
-        if (x_diff >= C.centerThresh) {
-          System.out.println("INSIDE NOT CENTERED ----------------------\n");
-          
-          // Centering a red block if it enters into frame
-          DESIRED[0] = -45;
-          DESIRED[1] = 45;
-          DESIRED[2] = 135;
-          DESIRED[3] = 225;
-
-          setpoint = C.vision_kP * (centerx - x) * ((C.Trans_maxRPM / 6) / C.MAX_rad_s);
-          for (int i = 0; i <=3; i++) {
-            if (i % 2 == 0) {
-              setpointArr[i] = -1 * setpoint;
-            }
-            else {
-              setpointArr[i] = setpoint;
-            }    
-          }
-        }
-
-        // if centered, move closer or farther away
-        else {
-
-          // if too far away, go closer
-          if (area <= C.minDistThresh) {
-            System.out.println("INSIDE TOO FAR ----------------------\n");
-
-            DESIRED[0] = 0;
-            DESIRED[1] = 0;
-            DESIRED[2] = 0;
-            DESIRED[3] = 0;
-
-            setpoint = 0.25 * (C.Trans_maxRPM / 6);
-            for (int i = 0; i <=3; i++) {
-              setpointArr[i] = setpoint;
-            }
-            
-          }
-
-          // if too close, go back
-          else if ((area > C.maxDistThresh) && x_diff < C.centerThresh) {
-            System.out.println("INSIDE TOO CLOSE ----------------------\n");
-
-            DESIRED[0] = 0;
-            DESIRED[1] = 0;
-            DESIRED[2] = 0;
-            DESIRED[3] = 0;
-
-            setpoint = -0.25 * (C.Trans_maxRPM / 6);
-            for (int i = 0; i <=3; i++) {
-              setpointArr[i] = setpoint;
-            }
-
-          } 
-
-          // otherwise, ideal spot achieved
-          else {
-            System.out.println("IDEAL SPOT ACHIEVED ----------------------");
-
-            // red-following demo
-            stopMotors();
-            currState = autoStates.findRed;
-
-            // red-green loop demo
-            // currState = autoStates.findGreen;
-
-            // etc.
-          }
-          
-        }
-
-        apply_offsets(desired_body, DESIRED, current_rel, desired_rel1, desired_translation);
-
-        setWheelState(RM_PIDControllers, desired_rel1, true, setpointArr);
-        setWheelState(TM_PIDControllers, desired_translation, false, setpointArr);
-
-      break;
-
-      case stopped:
-
-        System.out.println("currState = stopped ------------------------");
-      
-        stopMotors();
-      break;
-      
-      case cool:
-
-        double time = m_Timer.get();
-
-        // Constants
-        double dist_x = 12.75;
-        double dist_y = 12.75;
-        double r = mag(dist_x, dist_y);
-        
-        // assume omega = 0.1 rad/s
-        // at t=0, theta=0, robot should be moving in the [1, 0] direction
-        // at t=10*pi/2 seconds, theta=pi/2, robot should be moving in the [0, -1] direction
-        // at t=10*pi seconds, theta=pi, robot should be moving in the [-1, 0] direction 
-        double theta = time * cool_omega;
-
-        omega_auto = cool_omega;
-
-        Vx_auto = Math.cos(theta) * cool_speed;
-        Vy_auto = -Math.sin(theta) * cool_speed;
-
-        // calculate the vector sum of joystick inputs to define each respective wheel module's 
-        // linear and angular velocities
-        double[] state = set_vectors(Vx_auto, Vy_auto, omega_auto, dist_x, dist_y, r);
-
-        // in case 2, each wheel is set to a distinct angle and speed
-        for (int j = 0; j <= 3; j++) {
-          DESIRED[j] = state[j];
-          setpointArr[j] = state[j + 4];
-        }
-
-        apply_offsets(desired_body, DESIRED, current_rel, desired_rel1, desired_translation);
-
-        setWheelState(RM_PIDControllers, desired_rel1, true, setpointArr);
-        setWheelState(TM_PIDControllers, desired_translation, false, setpointArr);
-
-      break;
-
-      default:
-        break;
-
-    }
-
+    System.out.println(redArea);
   }
+
+  /** This function is called periodically during autonomous. */
+  // @Override
+  // public void autonomousPeriodic() {
+
+  //   // Network Tables - ere
+  //   double redArea = redASub.get();
+  //   double redX = redXSub.get();
+  //   double redY = redYSub.get();
+
+  //   System.out.println(redArea);
+
+
+  //   double[] current_rel = new double[] {
+  //       RM_Encoders.get(WHEEL_FL).getPosition(),
+  //       RM_Encoders.get(WHEEL_FR).getPosition(),
+  //       RM_Encoders.get(WHEEL_BR).getPosition(),
+  //       RM_Encoders.get(WHEEL_BL).getPosition()
+  //   };
+
+  //   // Initialize empty arrays for RM and TM setPoint values to be used later by
+  //   // setReference
+  //   double[] desired_body = new double[] { 0.0, 0.0, 0.0, 0.0 };
+  //   double[] desired_rel1 = new double[] { 0.0, 0.0, 0.0, 0.0 };
+  //   double[] desired_translation = new double[] { 0.0, 0.0, 0.0, 0.0 };
+  //   double[] DESIRED = new double[] { 0, 0, 0, 0 };
+
+  //   Point center = cv.rectCenter(cv.biggestRed);
+
+  //   if (colorOfInterest == Colors.red) {
+  //     center = cv.rectCenter(cv.biggestRed);
+  //   }
+  //   else if (colorOfInterest == Colors.green) {
+  //     center = cv.rectCenter(cv.biggestGreen);
+  //   }
+
+  //   double x = center.x;
+  //   double centerx = (double) cv.imgWidth / 2;
+  //   double x_diff = Math.abs(centerx - x);
+    
+  //   double setpoint = 0;
+  //   double[] setpointArr = new double[] {setpoint, setpoint, setpoint, setpoint};
+
+  //   switch (currState) {
+
+  //     case findRed:
+  //       System.out.println("currState = findRed ------------------------");
+      
+  //       colorOfInterest = Colors.red;
+
+  //       // if nothing of (RED) color detected, keep spinning around until detected
+  //       if (cv.biggestRed.area() <= C.minSpinThresh) {
+  //         spin(DESIRED, desired_body, desired_rel1, desired_translation, current_rel);
+  //       }
+  //       else {
+  //         currState = autoStates.move;
+  //       }
+  //     break;
+
+  //     case findGreen:
+  //       System.out.println("currState = findGreen ------------------------");
+        
+  //       colorOfInterest = Colors.green;
+
+  //       // if nothing of (GREEN) color detected, keep spinning around until detected
+  //       if (cv.biggestGreen.area() <= C.minSpinThresh) {
+  //         spin(DESIRED, desired_body, desired_rel1, desired_translation, current_rel);
+  //       }
+  //       else {
+  //         currState = autoStates.move;
+  //       }
+  //     break;
+
+  //     case move:
+
+  //       System.out.println("currState = move ------------------------");
+  //       System.out.printf("colorOfInterest = %s ------------------------\n", colorOfInterest);
+  //       System.out.printf("area = %f ------------------------\n", area);
+      
+          
+  //       if (colorOfInterest == Colors.red) {
+  //         area = cv.biggestRed.area();
+  //       }
+  //       else if (colorOfInterest == Colors.green) {
+  //         area = cv.biggestGreen.area();
+  //       }
+        
+  //       // if not centered
+  //       if (x_diff >= C.centerThresh) {
+  //         System.out.println("INSIDE NOT CENTERED ----------------------\n");
+          
+  //         // Centering a red block if it enters into frame
+  //         DESIRED[0] = -45;
+  //         DESIRED[1] = 45;
+  //         DESIRED[2] = 135;
+  //         DESIRED[3] = 225;
+
+  //         setpoint = C.vision_kP * (centerx - x) * ((C.Trans_maxRPM / 6) / C.MAX_rad_s);
+  //         for (int i = 0; i <=3; i++) {
+  //           if (i % 2 == 0) {
+  //             setpointArr[i] = -1 * setpoint;
+  //           }
+  //           else {
+  //             setpointArr[i] = setpoint;
+  //           }    
+  //         }
+  //       }
+
+  //       // if centered, move closer or farther away
+  //       else {
+
+  //         // if too far away, go closer
+  //         if (area <= C.minDistThresh) {
+  //           System.out.println("INSIDE TOO FAR ----------------------\n");
+
+  //           DESIRED[0] = 0;
+  //           DESIRED[1] = 0;
+  //           DESIRED[2] = 0;
+  //           DESIRED[3] = 0;
+
+  //           setpoint = 0.25 * (C.Trans_maxRPM / 6);
+  //           for (int i = 0; i <=3; i++) {
+  //             setpointArr[i] = setpoint;
+  //           }
+            
+  //         }
+
+  //         // if too close, go back
+  //         else if ((area > C.maxDistThresh) && x_diff < C.centerThresh) {
+  //           System.out.println("INSIDE TOO CLOSE ----------------------\n");
+
+  //           DESIRED[0] = 0;
+  //           DESIRED[1] = 0;
+  //           DESIRED[2] = 0;
+  //           DESIRED[3] = 0;
+
+  //           setpoint = -0.25 * (C.Trans_maxRPM / 6);
+  //           for (int i = 0; i <=3; i++) {
+  //             setpointArr[i] = setpoint;
+  //           }
+
+  //         } 
+
+  //         // otherwise, ideal spot achieved
+  //         else {
+  //           System.out.println("IDEAL SPOT ACHIEVED ----------------------");
+
+  //           // red-following demo
+  //           stopMotors();
+  //           currState = autoStates.findRed;
+
+  //           // red-green loop demo
+  //           // currState = autoStates.findGreen;
+
+  //           // etc.
+  //         }
+          
+  //       }
+
+  //       apply_offsets(desired_body, DESIRED, current_rel, desired_rel1, desired_translation);
+
+  //       setWheelState(RM_PIDControllers, desired_rel1, true, setpointArr);
+  //       setWheelState(TM_PIDControllers, desired_translation, false, setpointArr);
+
+  //     break;
+
+  //     case stopped:
+
+  //       System.out.println("currState = stopped ------------------------");
+      
+  //       stopMotors();
+  //     break;
+      
+  //     case cool:
+
+  //       double time = m_Timer.get();
+
+  //       // Constants
+  //       double dist_x = 12.75;
+  //       double dist_y = 12.75;
+  //       double r = mag(dist_x, dist_y);
+        
+  //       // assume omega = 0.1 rad/s
+  //       // at t=0, theta=0, robot should be moving in the [1, 0] direction
+  //       // at t=10*pi/2 seconds, theta=pi/2, robot should be moving in the [0, -1] direction
+  //       // at t=10*pi seconds, theta=pi, robot should be moving in the [-1, 0] direction 
+  //       double theta = time * cool_omega;
+
+  //       omega_auto = cool_omega;
+
+  //       Vx_auto = Math.cos(theta) * cool_speed;
+  //       Vy_auto = -Math.sin(theta) * cool_speed;
+
+  //       // calculate the vector sum of joystick inputs to define each respective wheel module's 
+  //       // linear and angular velocities
+  //       double[] state = set_vectors(Vx_auto, Vy_auto, omega_auto, dist_x, dist_y, r);
+
+  //       // in case 2, each wheel is set to a distinct angle and speed
+  //       for (int j = 0; j <= 3; j++) {
+  //         DESIRED[j] = state[j];
+  //         setpointArr[j] = state[j + 4];
+  //       }
+
+  //       apply_offsets(desired_body, DESIRED, current_rel, desired_rel1, desired_translation);
+
+  //       setWheelState(RM_PIDControllers, desired_rel1, true, setpointArr);
+  //       setWheelState(TM_PIDControllers, desired_translation, false, setpointArr);
+
+  //     break;
+
+  //     default:
+  //       break;
+
+  //   }
+
+  // }
 
   /**
    * This function is called once each time the robot enters teleoperated mode.
--- src/main/java/frc/robot/vision.py
+++ src/main/java/frc/robot/vision.py
@@ -19,8 +19,6 @@ redlow2 = (160, 70, 50)
 redhigh2 = (179, 255, 255)
 
 if __name__ == "__main__":
-    global configFile, GB
-    global redlow, redhigh, redlow2, redhigh2
     print(cv2.__version__)
     with open(configFile) as f:
         config = json.load(f)
@@ -31,13 +29,18 @@ if __name__ == "__main__":
     print("Starting camera '{}' on {}".format(camera_config["name"], camera_config["path"]))
 
     nt = ntcore.NetworkTableInstance.getDefault()
+    # nt.setSeverTeam(6593)
+    nt.startClient4("rasp pi client")
+    nt.setServerTeam(6593)
+    nt.startDSClient()
+    nt.setServer("host", ntcore.NetworkTableInstance.kDefaultPort4)
 
     # camera.getProperty("name").set(val)
     camera = CameraServer.startAutomaticCapture()
     camera.setResolution(WIDTH, HEIGHT)
     camera.setBrightness(60)
     camera.setWhiteBalanceManual(3300)
-    camera.setExposureManual(7)
+    camera.setExposureManual(1)
     camera.getProperty("contrast").set(50)
     camera.getProperty("saturation").set(80)
     camera.getProperty("focus_auto").set(0)
@@ -46,8 +49,13 @@ if __name__ == "__main__":
     output_stream = CameraServer.putVideo("Processed", WIDTH, HEIGHT)
 
     vision_nt = nt.getTable("Vision")
+    biggestArea = vision_nt.getDoubleTopic("redA").publish()
+    biggestX = vision_nt.getDoubleTopic("redX").publish()
+    biggestY = vision_nt.getDoubleTopic("redY").publish()
 
     img = np.zeros((WIDTH, HEIGHT, 3), dtype=np.uint8)
+
+    counter = 0
 
     time.sleep(0.5)
 
@@ -60,31 +68,33 @@ if __name__ == "__main__":
 
             hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-            blur = cv2.GaussianBlur(hsv,gb,0)
+            blur = cv2.GaussianBlur(hsv,GB,0)
 
             redmask1 = cv2.inRange(blur, redlow, redhigh)
             redmask2 = cv2.inRange(blur, redlow2, redhigh2)
 
             redmask = cv2.bitwise_or(redmask1, redmask2)
 
-            _, contours, hierarchy = cv2.findContours(redmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            contours, hierarchy = cv2.findContours(redmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             # cv2.drawContours(output_img, contours, -1, (0,255,0), 3)
 
-            biggestArea = 0
-            biggestX = 0
-            biggestY = 0
-            biggestH = 0
-            biggestW = 0
+            biggestAreaTemp = 0.0
+            biggestXTemp = 0.0
+            biggestYTemp = 0.0
+            biggestHTemp = 0.0
+            biggestWTemp = 0.0
             for c in contours:
                 x,y,w,h = cv2.boundingRect(c)
                 cv2.rectangle(image_o2,(x,y),(x+w,y+h),(0,255,0),2)
                 a = w*h
+                centerx = x + (w/2)
+                centery = y + (h/2)
                 if a > biggestArea:
-                    biggestArea = a
-                    biggestX = x
-                    biggestY = y
-                    biggestH = h
-                    biggestW = w
+                    biggestAreaTemp = a
+                    biggestXTemp = centerx
+                    biggestYTemp = centery
+                    biggestHTemp = h
+                    biggestWTemp = w
             
             # cv2.rectangle(output_img,(biggestX,biggestY),(biggestX+biggestW,biggestY+biggestH),(0,255,0),2)
 
@@ -94,10 +104,14 @@ if __name__ == "__main__":
             # fps = 1/process_time
             # cv2.putText(output_img, str(round(fps, 1)), (0, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255))
 
+            counter = counter + 1
+            biggestAreaTemp = counter
+            
             output_stream.putFrame(redmask)
-            vision_nt.putNumber("Red Area", biggestArea)
-            vision_nt.putNumber("Red X", biggestX + (biggestW/2))
-            vision_nt.putNumber("Red Y", biggestY + (biggestH/2))
+            biggestArea.set(biggestAreaTemp)
+            biggestX.set(biggestXTemp)
+            biggestY.set(biggestYTemp)
+            
 
 #   JSON format:
 #   {
